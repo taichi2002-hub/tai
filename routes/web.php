afa5952f4965255b433157778150d22552fa8611
taichi2002-hub <taichiyuzumikan310@icloud.com>
2022-11-24 09:42:28 +0000

    こんにちは

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,8 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/posts', [PostController::class, 'index']); 
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class ,'create']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
+Route::post('/posts', [PostController::class ,'store']);
+?>
